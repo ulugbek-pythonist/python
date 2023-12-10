624f6b760eab8066fdd109f7c6adffe58f142821
ulugbekpy <ulugbekpycpp@gmail.com>
2023-12-11 00:12:39 +0500

    4-dars

---
+++ basics/start4.py
@@ -0,0 +1,21 @@
+# 5-7 min
+
+# user input
+
+
+# ism = input("Ismingiz: ")
+
+# print(type(ism))
+
+# print("Assalomu alaykum, " + ism)
+
+son = input("Son kiriting: ")
+son = int(son)
+print(son * 2)
+# "dev" + "eloper" = "developer"
+# 3 + 4 = 7  
+# "3" + "40" = 340
+# "3"+"3"
+
+# Foydalanuvchidan yoshini kiritishini so'rang 
+# va qachon tug'ilganini aytib bering
