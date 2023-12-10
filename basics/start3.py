3769fa378f58634798c8f2a9833aa6e534e8ec01
ulugbekpy <ulugbekpycpp@gmail.com>
2023-12-11 00:03:04 +0500

    3-dars

---
+++ basics/start3.py
@@ -0,0 +1,25 @@
+# Tur o'zgartirish (type conversion)
+
+# int --> float/str/bool
+
+# a = 0
+# a = bool(a)
+# print(a)
+
+# float --> int/str/bool
+
+# bool --> str/int/float
+
+# b = False
+
+# b = float(b)
+
+# print(b)
+
+# str  --> bool/int/float
+
+a = ""
+a = bool(a)
+print(a)
+
+# 5-7
