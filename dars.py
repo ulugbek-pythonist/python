da1e1abe02d8af4ca9a55f9bcf5d85a6c5a93352
ulugbekpy <ulugbekpycpp@gmail.com>
2024-01-03 10:54:15 +0500

    day 2

---
+++ dars.py
@@ -0,0 +1,66 @@
+# import time
+from time import sleep
+
+# print("maktab",end="\r")
+# sleep(3)
+# print("kollej")
+# ctrl + s  (saqlash)
+
+# print("Loading 25%",end="\r")
+# sleep(1.2)
+# print("Loading 50%",end="\r")
+# sleep(1.2)
+# print("Loading 75%",end="\r")
+# sleep(1.2)
+# print("Downloaded")
+
+# print("Men dasturchi",True,"Pythonda yozaman",1.5,sep="-")
+
+# ism = "Abdughani"
+
+# print(ism,"gmail.com",sep="@")
+
+
+# uning ismi = "Bobur" ? 
+
+# a-z _ 0-9
+
+# 3r = 2 ?
+
+# __dev = 3
+
+# print(__dev + 3)
+
+
+# ism = "a"
+
+# print(type(ism))
+
+# ism = int(ism)
+
+# balance = "500"
+# balance = int(balance) # "500" -> 500
+# print(balance + 200)
+
+# rating = -4.7
+
+# print(int(rating))
+
+temp = 0.0001
+
+temp = bool(temp)
+
+print(temp)
+
+# int()
+# float()
+# bool()
+# str()
+
+# debt = -4.5
+
+# debt = str(debt) # -4.5 -> "-4.5"
+
+# print(debt + "21")
+
+
