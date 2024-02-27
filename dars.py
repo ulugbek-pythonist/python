4d3a1571fb1f0ca343badf7f288c38847237a0aa
ulugbekpy <ulugbekpycpp@gmail.com>
2024-02-27 17:07:22 +0500

    3-dars

--- dars.py
+++ dars.py
@@ -1,66 +1,18 @@
-# import time
-from time import sleep
+text = "Some text"
+# print("DOiMo".lower())
 
-# print("maktab",end="\r")
-# sleep(3)
-# print("kollej")
-# ctrl + s  (saqlash)
+# print("PDP academy".upper())
 
-# print("Loading 25%",end="\r")
-# sleep(1.2)
-# print("Loading 50%",end="\r")
-# sleep(1.2)
-# print("Loading 75%",end="\r")
-# sleep(1.2)
-# print("Downloaded")
+# 1 (.)nuqta dan keyin
+# 2 metod nomidan keyin qavs qo'yiladi ()
+# print(type(text))
 
-# print("Men dasturchi",True,"Pythonda yozaman",1.5,sep="-")
-
-# ism = "Abdughani"
-
-# print(ism,"gmail.com",sep="@")
-
-
-# uning ismi = "Bobur" ? 
-
-# a-z _ 0-9
-
-# 3r = 2 ?
-
-# __dev = 3
-
-# print(__dev + 3)
-
-
-# ism = "a"
-
-# print(type(ism))
-
-# ism = int(ism)
-
-# balance = "500"
-# balance = int(balance) # "500" -> 500
-# print(balance + 200)
-
-# rating = -4.7
-
-# print(int(rating))
-
-temp = 0.0001
-
-temp = bool(temp)
-
-print(temp)
-
-# int()
-# float()
-# bool()
-# str()
-
-# debt = -4.5
-
-# debt = str(debt) # -4.5 -> "-4.5"
-
-# print(debt + "21")
-
-
+# print("Toshkent".swapcase())
+# print("andiJon ShAhri".capitalize())
+# print("kiChik jumLa".title())
+# print("telefon raqam".count("R"))
+# print("telefon raqam".index("R"))
+# print("telefon raqam".find("R"))
+# print("   . ".isspace())
+# print("37816382/".isdigit())
+# print("dnadn>sda".isalpha())
