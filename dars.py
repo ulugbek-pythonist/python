65a597c63ead981391f819dee4e795723fcb19a5
ulugbekpy <ulugbekpycpp@gmail.com>
2024-03-05 12:11:27 +0500

    4-dars

--- dars.py
+++ dars.py
@@ -1,18 +1,67 @@
-text = "Some text"
-# print("DOiMo".lower())
+# # arifmetik: + - * /  %  // **
+# a = -10
+# b = 3
 
-# print("PDP academy".upper())
+# print(a % b) # 1
 
-# 1 (.)nuqta dan keyin
-# 2 metod nomidan keyin qavs qo'yiladi ()
-# print(type(text))
+# print(a // b) # 3         3.5 -> 3      -3.5 -> -4
+# print(2**7)
 
-# print("Toshkent".swapcase())
-# print("andiJon ShAhri".capitalize())
-# print("kiChik jumLa".title())
-# print("telefon raqam".count("R"))
-# print("telefon raqam".index("R"))
-# print("telefon raqam".find("R"))
-# print("   . ".isspace())
-# print("37816382/".isdigit())
-# print("dnadn>sda".isalpha())
+# assignment: =; +=; -=; /=; *=; %=; **=; ...
+# import time
+# speed = 20 # 20
+
+# speed += 3 # 20 + 3 = 23
+
+# time.sleep(1)
+# print(speed)
+# speed = speed + 3
+# time.sleep(1)
+# print(speed)
+# speed = speed + 3
+# time.sleep(1)
+# print(speed)
+# speed = speed + 3
+# time.sleep(1)
+# print(speed)
+# speed = speed + 3
+
+# a = 4
+# print(a)
+# a %= 3 # a = a % 3 | a = 4 % 3 = 1
+# print(a)
+# # a += 1 # a = a + 1
+# a -= 1 # a = a - 1
+
+# a = 5
+# b = 6
+# print(a != b)
+# > < >= <= == !=
+# is; is not
+
+# x = [10,11]
+# y = [10,11]
+# print(id(x))
+# print(id(y))
+# print(x is not y)
+
+# in; not in
+# someone = "Tohir"
+# users = ["alisher","dono","tohir"]
+# print("Tohir"=="tohir")
+# case-sensitive  & case-insensetive
+
+# _Speed = 3
+# _speed = 4
+# print(Speed)
+# print(speed)
+
+
+# print(someone in users)
+
+# logical operators: and; or; not
+
+# print(True and False) # 1*0 = 0
+# print(10 > 9 and len("ulugbek")>5)
+# print(True or False) # 
+# print(not False)
